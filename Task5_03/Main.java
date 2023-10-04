09da9e40d817dc166c62a5b60e4d13046de2cc47
Kseniia <betrezen145@gmail.com>
2023-10-04 15:09:45 +0300

    Update Main.java

--- Task5_03/Main.java
+++ Task5_03/Main.java
@@ -1,7 +1,3 @@
-//Створіть ConverterTemperature з методом convert.
-//Реалізуйте для ConverterTemperature класи для конвертації з градусів за Цельсієм в Кельвіни, Фаренгейти.
-// У класу має бути метод convert, який робить конвертацію.
-//Інстанс ConverterTemperature створити неможливо.
 package Task5_03;
 
 public class Main {
